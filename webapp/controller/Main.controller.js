b10f35554a47e1a8af8b09a44b1cee4506ca1e1f
Rafa <rafagmez@gmail.com>
2024-10-20 16:11:52 +0000

    JSON y CARGAR JSON

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,11 +1,24 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel" // Asegúrate de importar JSONModel
 ],
-function (Controller) {
+function (Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("invoice.controller.Main", {
         onInit: function () {
+
+            // Corrección: Es 'JSONModel' y no 'oJSONModel'
+            const oJSONModel = new JSONModel();
+
+            // Obtiene la vista
+            const oView = this.getView();
+
+            // Carga el archivo JSON
+            oJSONModel.loadData("./model/selectionScreenMenu.json");
+
+            // Asigna el modelo a la vista
+            oView.setModel(oJSONModel, "selectionScreen");
 
         }
     });
