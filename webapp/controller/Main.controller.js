c6e62762fced4694f86e1d9b4da1c100e19815a7
Rafa <rafagmez@gmail.com>
2024-10-21 13:16:29 +0000

    Lógica botón filtro

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,8 +1,18 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel" // Asegúrate de importar JSONModel
+    "sap/ui/model/json/JSONModel", // Asegúrate de importar JSONModel
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+
 ],
-function (Controller, JSONModel) {
+/**
+ * 
+ * @param {typeof sap.ui.core.mvc.Controller} Controller 
+ * @param {typeof sap.ui.core.mvc.Filter} Filter 
+ * @param {typeof sap.ui.core.mvc.FilterOperator} FilterOperator 
+ * @returns 
+ */
+function (Controller, JSONModel, FilterOperator) {
     "use strict";
 
     return Controller.extend("invoice.controller.Main", {
@@ -16,19 +26,33 @@ function (Controller, JSONModel) {
             // Asigna el modelo a la vista
             oView.setModel(oJSONModel, "selectionScreen");
 
-             // 2. Manejo del fallo en la carga de metadatos (OData)
-            //const oModel = this.getOwnerComponent().getModel();
-            //oModel.attachMetadataFailed(function(oEvent) {
-                //console.error("Metadata loading failed", oEvent.getParameters());
-
         },
         onFilter: function(onEvent)  {
+
+            const oData = this.getView().getModel("selectionScreen").getData();
+            let filters = [];
+
+            if (oData.ShipName !== ""){
+                filters.push(new Filter("ShipName", FilterOperator.Contains, oData.ShipName ));
+            }
+
+            if (oData.Countrykey !== ""){
+                filters.push(new Filter("Country", FilterOperator.EQ, oData.Countrykey ));
+            }
+
+            const oList = this.getView().byId("invoicesList");
+            const oBinding =  oList.getBinding("items");
+            oBinding.filter(filters);
             
         } ,
         onClearFilter: function(){
             const oModelSS =this.getView().getModel("selectionScreen");
             oModelSS.setProperty("/CountryKey", "");
             oModelSS.setProperty("/ShipName", "");
+
+            const oList = this.getView().byId("invoicesList");
+            const oBinding =  oList.getBinding("items");
+            oBinding.filter([]);
         }
     });
 });
